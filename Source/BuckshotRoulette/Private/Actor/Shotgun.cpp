1f55569374eb3816323ff55f54f32c2ba54de98e
nimnoeyheel <hyeonmin1012@gmail.com>
2025-06-01 01:27:20 +0900

    [Chore] Shotgun 에셋 추가, 액터 클래스 생성
    - Shotgun 스켈레탈 메시 컴포넌트 선언 및 메시 애셋 로드
    - OverlapBox 컴포넌트 생성 및 위치와 크기 지정

--- Source/BuckshotRoulette/Private/Actor/Shotgun.cpp
+++ Source/BuckshotRoulette/Private/Actor/Shotgun.cpp
@@ -1,7 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Actor/Shotgun.h"
+#include "Components/BoxComponent.h"
+#include "Engine/SkeletalMesh.h"
 
 // Sets default values
 AShotgun::AShotgun()
@@ -9,6 +11,19 @@ AShotgun::AShotgun()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ShotgunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ShotgunMesh"));
+	RootComponent = ShotgunMesh;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ShotGunMeshAsset(TEXT("/Game/CGrShotgunsPack/Weapons/Meshes/Shotgun_Meshes/SKM_SAShotgun.SKM_SAShotgun"));
+	if (ShotGunMeshAsset.Object)
+	{
+		ShotgunMesh->SetSkeletalMesh(ShotGunMeshAsset.Object);
+	}
+
+	OverlapBox = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapBox"));
+	OverlapBox->SetupAttachment(RootComponent);
+	OverlapBox->SetRelativeLocation(FVector(30, 0, 5)); // (X=30.000000,Y=0.000000,Z=5.000000)
+	OverlapBox->SetRelativeScale3D(FVector(1.6f, 0.1f, 0.2f)); // (X=1.600000,Y=0.100000,Z=0.200000)
 }
 
 // Called when the game starts or when spawned
@@ -24,4 +39,3 @@ void AShotgun::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
--- Source/BuckshotRoulette/Public/Actor/Shotgun.h
+++ Source/BuckshotRoulette/Public/Actor/Shotgun.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -23,4 +23,11 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class USkeletalMeshComponent* ShotgunMesh;
+
+	// 오버랩 영역 (상호작용)
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* OverlapBox;
 };
