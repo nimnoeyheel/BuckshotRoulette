04cbccbefcbcc9dc3cf20c8a53d3dd3779458e2e
nimnoeyheel <hyeonmin1012@gmail.com>
2025-06-05 17:07:51 +0900

    [Feat] 프랍, 캐릭터 애셋 및 애니메이션 추가
    - BPCharacter의 스켈레탈메시 추가한 캐릭터로 설정

--- Source/BuckshotRoulette/Private/Character/BRCharacter.cpp
+++ Source/BuckshotRoulette/Private/Character/BRCharacter.cpp
@@ -10,6 +10,14 @@ ABRCharacter::ABRCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshPath (TEXT("/Game/MANIACS/ManiacKiller/Mesh/Maniac4/SK_maniac_killer_4.SK_maniac_killer_4"));
+	if (MeshPath.Object)
+	{
+		GetMesh()->SetSkeletalMesh(MeshPath.Object);
+		GetMesh()->SetRelativeLocation(FVector(0, 0, -90)); //(X=0.000000,Y=0.000000,Z=-90.000000)
+		GetMesh()->SetRelativeRotation(FRotator(0, -90, 0)); //(Pitch=0.000000,Yaw=-90.000000,Roll=0.000000)
+	}
+
 }
 
 // Called when the game starts or when spawned
